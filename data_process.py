14cac13f9fae618eb01717f8d311eaedc3dc6c14
dengzengtian <dengcengtian@dengceniandembp.home>
2018-11-15 11:40:36 -0500

    Complete program code added

---
+++ data_process.py
@@ -0,0 +1,85 @@
+from sendgrid.helpers.mail import *
+
+
+def tachycardic(hr, age):
+    """
+
+    Parameters
+    ----------
+    hr
+    age
+
+    Returns
+    -------
+
+    """
+    out = "Negative"
+    if age == 1 or age == 2:
+        thresh = 151
+    elif age == 3 or age == 4:
+        thresh = 137
+    elif age >= 5 and age <= 7:
+        thresh = 133
+    elif age >= 8 and age <= 11:
+        thresh = 130
+    elif age >= 12 and age <= 15:
+        thresh = 119
+    elif age > 15:
+        thresh = 100
+    if int(hr) > thresh:
+        out = "Positive"
+        return out
+    else:
+        return out
+
+
+def avg_hr(hr):
+    """
+
+    Parameters
+    ----------
+    hr
+
+    Returns
+    -------
+
+    """
+    import numpy as np
+    avg = np.mean(hr)
+    return avg
+
+
+def get_intv_avg(database, time):
+    import numpy as np
+    import datetime
+    reftime = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
+    hr = []
+    index = 0
+    for i in database["Timestamp"]:
+        if i >= reftime:
+            hr.append(database["HR_list"][index])
+        index += 1
+    if len(hr) == 0:
+        raise IndexError
+    avg = np.mean(hr)
+    return avg
+
+
+def send_an_email(info):
+    import sendgrid
+    import os
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("zd32@duke.edu")
+    to_email = Email(info["attending_email"])
+    subject = "Tachycardic symptom detected in patient {0}!".format(
+        info["patient_id"])
+    content = Content("text/plain", "An abnormal heart rate: {0} bpm,"
+                                    " is observed from patient {1} at"
+                                    " time {2}, which conforms with "
+                                    "the symptom of tachycardia".format(
+                                        info["HR_list"][-1],
+                                        info["patient_id"],
+                                        info["Timestamp"][-1]
+                                    ))
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
---
+++ main.py
@@ -0,0 +1,184 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+import datetime
+
+app = Flask(__name__)
+database = {}
+
+
+@app.route("/api/new_patient", methods=["POST"])
+def new_patient():
+    """
+    Function that stores a new patient into the system based on user input
+
+    Returns
+    -------
+    flag: int
+        Integer that indicates whether a new patient is added
+        1 means added, 0 means not added
+    """
+    from request_validation import validpatient
+    info = request.get_json()
+    try:
+        validpatient(info)
+        info["patient_id"] = str(info["patient_id"])
+        info["user_age"] = int(info["user_age"])
+        key = info["patient_id"]
+        info["HR_list"] = []
+        info["Timestamp"] = []
+        info["heart_rate_average_since"] = ""
+        database[key] = info
+        return "Submission Accepted"
+    except NameError:
+        return jsonify({"NameError":
+                        "One or more essential entry missing"})
+    except ValueError:
+        return jsonify({"ValueError":
+                        "One or more entries"
+                        " contain unexpected value"})
+
+
+@app.route("/api/heart_rate", methods=["POST"])
+def new_hr():
+    """
+
+    Returns
+    -------
+
+    """
+    from request_validation import validhr
+    from data_process import tachycardic
+    from data_process import send_an_email
+    info = request.get_json()
+    try:
+        validhr(info, database)
+        time = datetime.datetime.now()
+        key = str(info["patient_id"])
+        database[key]["HR_list"].append(int(info["heart_rate"]))
+        database[key]["Timestamp"].append(time)
+        status = tachycardic(info["heart_rate"], database[key]["user_age"])
+        if status == "Positive":
+            send_an_email(database[key])
+        return "Submission Accepted"
+    except NameError:
+        return jsonify({"NameError":
+                        "One or more essential entry missing"})
+    except ValueError:
+        return jsonify({"ValueError":
+                        "One or more entries"
+                        " contain unexpected value"})
+
+
+@app.route("/api/heart_rate/interval_average", methods=["POST"])
+def avg_interval():
+    from request_validation import validtime
+    from request_validation import validid
+    from data_process import get_intv_avg
+    info = request.get_json()
+    try:
+        validid(info["patient_id"], database)
+        key = str(info["patient_id"])
+        try:
+            validtime(info, database)
+            avg = get_intv_avg(database[key], info["heart_rate_average_since"])
+            return jsonify({"avg_HR": avg,
+                            "since": info["heart_rate_average_since"]})
+        except IndexError:
+            return jsonify({
+                "Error": "Specified time is larger "
+                         "than all timestamps of this patient",
+            })
+        except KeyError:
+            return jsonify({
+                "KeyError": "Invalid time entry"
+            })
+        except ValueError:
+            return jsonify({
+                "ValueError": "Patient does not have any record"
+            })
+    except IndexError:
+        return jsonify({
+            "IndexError": "Patient does not exist"
+        })
+    except ValueError:
+        return jsonify({
+            "ValueError": "Patient does not have any record"
+        })
+
+
+@app.route("/api/status/<patient_id>", methods=["GET"])
+def get_stat(patient_id):
+    """
+
+    Parameters
+    ----------
+    patient_id
+
+    Returns
+    -------
+
+    """
+    from request_validation import validid
+    from data_process import tachycardic
+    try:
+        validid(patient_id, database)
+        patient_id = str(patient_id)
+        state = tachycardic(database[patient_id]["HR_list"][-1],
+                            database[patient_id]["user_age"])
+        return jsonify({
+            "state": state,
+            "Timestamp": database[patient_id]["Timestamp"][-1],
+        })
+    except IndexError:
+        return jsonify({
+            "Error": "Patient does not exist"
+        })
+    except ValueError:
+        return jsonify({
+            "Error": "Patient does not have any record"
+        })
+
+
+@app.route("/api/heart_rate/<patient_id>", methods=["GET"])
+def get_hr(patient_id):
+    from request_validation import validid
+    try:
+        validid(patient_id, database)
+        patient_id = str(patient_id)
+        return jsonify({
+            "HR_list": database[str(patient_id)]["HR_list"],
+        })
+    except IndexError:
+        return jsonify({
+            "Error": "Patient does not exist"
+        })
+    except ValueError:
+        return jsonify({
+            "Error": "Patient does not have any record"
+        })
+
+
+@app.route("/api/heart_rate/average/<patient_id>", methods=["GET"])
+def get_avg_hr(patient_id):
+    from request_validation import validid
+    from data_process import avg_hr
+    try:
+        validid(patient_id, database)
+        patient_id = str(patient_id)
+        result = avg_hr(database[patient_id]["HR_list"])
+        return jsonify({
+            "avg_HR": result,
+        })
+    except IndexError:
+        return jsonify({
+            "Error": "Patient does not exist"
+        })
+    except ValueError:
+        return jsonify({
+            "Error": "Patient does not have any record"
+        })
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1")
